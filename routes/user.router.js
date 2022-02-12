7a76f50f7294316ba57bb13e4dde43248ea0e489
Graciele Damasceno <56450271+GracieleDamasceno@users.noreply.github.com>
2022-02-12 11:17:05 -0300

    feat: adding encryption to passwords
    ... and turning methods async! Brilliant!

--- routes/user.router.js
+++ routes/user.router.js
@@ -1,24 +1,51 @@
 const express = require("express");
 const router = express.Router();
+const bcrypt = require("bcryptjs");
 
 const User = require("../model/user.model.js");
 
-router.post("/account/join", (req, res) => {
+router.post("/account/join", async (req, res) => {
     console.log("[LOG] - Request to create a new user: ", req.body)
-    const user = new User({
-        username: req.body.username,
-        fullName: req.body.fullName,
-        email: req.body.email,
-        password: req.body.password
-    });
+    try{
+        const user = await User.findOne({email:req.body.email});
+        if(!user){
+            const hashedPassword = await bcrypt.hash(req.body.password, 10);
+            const insertUser = await User.create({
+                username: req.body.username,
+                fullName: req.body.fullName,
+                email: req.body.email,
+                password: hashedPassword,
+                joined: { type: Date, default: Date.now }
+            });
+            res.status(202).send(insertUser);
+        }else{
+            res.status(409).json({error:"E-mail address already registered"});
+        }
+       
+    } catch(error){
+        console.log("[LOG] - Error creating a new user: ", error);
+        res.status(500).json(error);
+    }
+});
 
-    user.save()
-        .then(result => {
-            res.status(202).json(result);
-        })
-        .catch(error => {
-            res.status(500).json(error);
-        });
+router.post("/account/sign-in", async (req, res) =>{
+    console.log("[LOG] - Request to log-in user: ", req.body.email)
+    try{
+        const user = await User.findOne({email:req.body.email});
+        if(user){
+            const comparison = await bcrypt.compare(req.body.password, user.password);
+            if(comparison){
+                res.status(200).json({message:"Successfully in!"});
+            }else{
+                res.status(401).json({error:"Wrong password or e-mail address"});
+            }
+        }else{
+            res.status(401).json({error:"Wrong password or e-mail address"});
+        }
+    }catch(error){
+        console.log("[LOG] - Error at log-in: ", error);
+        res.status(500).json(error);
+    }
 });
 
 module.exports = router;
