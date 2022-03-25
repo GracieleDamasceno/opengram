e25d85ddf4f41d4e45c8f173a6db6fe2ce383c93
Graciele Damasceno <56450271+GracieleDamasceno@users.noreply.github.com>
2022-03-25 18:31:38 -0300

    feat: adjusting multer's dynamic path

--- routes/album.routes.js
+++ routes/album.routes.js
@@ -9,14 +9,13 @@ var FormData = require('form-data');
 const Album = require("../model/album.model.js");
 const User = require("../model/user.model.js");
 var ObjectId = require('mongoose').Types.ObjectId;
-var albumLocation = "";
 
 const multerStorage = multer.diskStorage({
     destination: function (req, file, cb) {
         var dir = JSON.parse(req.body.albumInfo);
-        albumLocation = "E:/Public/Opengram/Albums/" + dir.albumNumber;
-        fs.mkdirSync(albumLocation, { recursive: true });
-        cb(null, albumLocation);
+        console.log(dir.albumLocation)
+        fs.mkdirSync(dir.albumLocation, { recursive: true });
+        cb(null, dir.albumLocation);
     },
     filename: function (req, file, cb) {
         cb(null, `${file.fieldname}-${Date.now()}${path.extname(file.originalname)}`);
@@ -27,13 +26,13 @@ const multerStorage = multer.diskStorage({
 const upload = multer({ storage: multerStorage });
 
 router.post("/album/create/", upload.single("photos"), async (req, res) => {
-    console.log("[LOG] - Request to upload photos: ", req.body.albumInfo);
+    console.log("[LOG] - Request to create album: ", req.body.albumInfo);
     const json = JSON.parse(req.body.albumInfo);
 
     const newAlbum = Album.create({
         albumName: json.albumName,
         albumDescription: json.albumDescription,
-        albumFolder: albumLocation,
+        albumFolder: json.albumLocation,
         albumThumbnail: req.file.filename,
         albumOwner: json.userId,
     });
@@ -44,6 +43,12 @@ router.post("/album/create/", upload.single("photos"), async (req, res) => {
     res.send(req.session);
     res.status(202).end();
 });
+
+router.post("/album", upload.any("photos"), async (req, res) => {
+    console.log("[LOG] - Request to upload photos: ", req.body.albumInfo);
+    res.status(202).end();
+});
+
 
 router.get("/album", async (req, res) => {
     var albums;
