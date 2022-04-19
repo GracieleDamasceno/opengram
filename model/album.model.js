2a06c5c845020adec4d35f3aea8a262c32a31a29
Graciele Damasceno <56450271+GracieleDamasceno@users.noreply.github.com>
2022-04-18 22:49:39 -0300

    refactor: restructuring photo and album's schema

--- model/album.model.js
+++ model/album.model.js
@@ -1,23 +1,29 @@
 const mongoose = require('mongoose');
-const { integer } = require('sharp/lib/is');
 const Schema = mongoose.Schema;
 
+const photoSchema = new Schema({
+    title: String,
+    description: String,
+    creationDate: { type: Date, default: Date.now },
+    thumbnailWidth: Number,
+    thumbnailHeight: Number
+});
+
+
 const albumSchema = new Schema({
-    albumName: String,
-    albumCreation: { type: Date, default: Date.now },
-    albumDescription: String,
-    albumFolder: String,
-    albumThumbnail: String,
-    albumThumbnailWidth: Number,
-    albumThumbnailHeight: Number,
-    albumOwner: {
+    name: String,
+    creationDate: { type: Date, default: Date.now },
+    description: String,
+    path: String,
+    thumbnail: String,
+    thumbnailWidth: Number,
+    thumbnailHeight: Number,
+    owner: {
         type: mongoose.Schema.Types.ObjectId,
         ref: "User"
     },
-    photos: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "Photo",
-    }
+    photos: [photoSchema]
 });
 
+module.exports = mongoose.model('Photo', photoSchema);
 module.exports = mongoose.model('Album', albumSchema);
